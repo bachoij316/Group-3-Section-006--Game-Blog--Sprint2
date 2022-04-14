bdeee93116bfa954f5c9bef36908d0d0fe1d5ebf
Ba Choi <bada@Bas-MacBook-Pro.mshome.net>
2022-04-14 00:31:29 -0400

    second

--- app.py
+++ app.py
@@ -297,5 +297,5 @@ def oauth2callback():
     """
 
 
-# app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
-app.run(debug=True)
+app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
+# app.run(debug=True)
